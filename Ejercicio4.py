138ebc6c64ca06215e1fcd3bfcfd6963720a2b13
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-10-25 23:58:47 +0200

    ejecución

---
+++ Ejercicio4.py
@@ -0,0 +1,8 @@
+ganados = int(input("Escribe el número de partidos ganados: "))
+print(" ")
+perdidos = int(input("Escribe el número de partidos perdidos: "))
+print(" ")
+empatados = int(input("Escribe el número de partidos empatados: "))
+print(" ")
+puntuacion_total = (ganados*3)+(perdidos*-1)+(empatados*0)
+print("La puntuación total es: "+str(puntuacion_total))
