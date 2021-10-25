804e82946734af5bfef5aea20c3b13a7398aaaba
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-10-26 00:16:56 +0200

    intentos

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,8 +1,7 @@
 #1 galón = 3,78541 litros
 
-un_galon_en_litros = 3,78541
 def coste_gasolina(total_galones):
-    total_a_pagar= total_galones*un_galon_en_litros
+    total_a_pagar= total_galones*3,78541
     return total_a_pagar
 
 print("El total a pagar en una gasolinara si llenas 10 galones es: "+str(coste_gasolina(10)))
