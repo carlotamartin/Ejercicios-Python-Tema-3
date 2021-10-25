890c66a39016b886622323b37c4009ef2a87b988
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-10-26 00:22:56 +0200

    realización

---
+++ EJERCICIO SUMAR, RESTAR, MULTIPLICAR Y DIVIDIR.py
@@ -0,0 +1,19 @@
+a = int ( input ())
+b = int ( input ())
+
+#La primera línea contiene la suma de los dos números.
+s =a+b
+print (s)
+#La segunda línea contiene la diferencia de los dos números (primero - segundo).
+d= a-b
+print (d)
+#La tercera línea contiene el producto de los dos números
+p=a*b
+print(p)
+#La cuarta línea debe contener el resultado de la división de enteros
+div_enteros = a//b
+print(div_enteros)
+#La quinta línea debe contener el resultado de la división flotante
+div_flotante = a/b
+print(div_flotante)
+
