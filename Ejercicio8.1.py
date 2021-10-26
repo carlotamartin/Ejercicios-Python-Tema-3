07acf4e8187237a34afc7cfd4bb991a0edd5f2a6
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-10-26 12:54:03 +0200

    cambios

---
+++ Ejercicio8.1.py
@@ -0,0 +1,14 @@
+import random
+
+num_aleatorio = random.randint(1,15)
+
+num_adivinado = input("Adivina un número entre el 1 y el 15")
+
+while(num_adivinado != num_aleatorio):
+    num_adivinado = input("Adivina un número entre el 1 y el 15")
+    intentos +=1
+    if(num_adivinado<num_aleatorio):
+        print ("El número es mayor")
+    else:
+        print ("El número es mayor")
+print ("Has acertado el número en el intento "+ str(intentos))
