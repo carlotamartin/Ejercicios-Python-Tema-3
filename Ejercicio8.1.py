cba6dcaa4b851412ededb1df6a3c63e7f6663b73
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-10-26 13:16:56 +0200

    cambios

--- Ejercicio8.1.py
+++ Ejercicio8.1.py
@@ -3,10 +3,13 @@ import random
 #Creamos un numero aleatorio
 num_aleatorio = random.randint(1,15)
 
+#Inicializamos el numero que introduciremos por teclado a 0 para que empiece a funcionar el buble
 num_adivinado = 0
+
+#Inicializamos los intentos a 0
 intentos =0
 
-
+#Creamos un bucle que vaya pidiendo numeros hasta que coincida con el numero aleatorio
 while(num_adivinado != num_aleatorio):
     num_adivinado = int(input("Adivina un numero entre el 1 y el 15: "))
     intentos +=1
