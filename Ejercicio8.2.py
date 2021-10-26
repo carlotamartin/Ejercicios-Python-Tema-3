5d7094f6cf4f025944cd4912c481db4d47d4c8f2
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-10-26 13:18:11 +0200

    eliminamos

--- Ejercicio8.2.py
+++ Ejercicio8.2.py
@@ -1,16 +1,22 @@
 import random
 
-num_aleatorio = random.randint(1,15)
-intentos = 0
+#Creamos un numero aleatorio
+num_aleatorio = random.randint(1,300)
 
-num_adivinado = input("Adivina un número entre el 1 y el 15")
+#Inicializamos el numero que introduciremos por teclado a 0 para que empiece a funcionar el buble
+num_adivinado = 0
+
+#Inicializamos los intentos a 0
+intentos =0
+
+#Creamos un bucle que vaya pidiendo numeros hasta que coincida con el numero aleatorio. Solos podrá acertar en 9 intentos
 for i in range (0,8):
-    if (num_adivinado != num_aleatorio):
-        num_adivinado = input("Adivina un número entre el 1 y el 15")
-    intentos +=1
-    if(num_adivinado<num_aleatorio):
-        print ("El número es mayor")
-    else:
-        print ("El número es mayor")
-    print ("Has acertado el número en el intento "+ str(intentos))
+    while(num_adivinado != num_aleatorio):
+        num_adivinado = int(input("Adivina un numero entre el 1 y el 300: "))
+        intentos +=1
+        if(num_adivinado<num_aleatorio):
+            print ("El numero es mayor")
+        else:
+            print ("El numero es menor")
 
+print ("Has acertado el numero " + str(num_aleatorio) + " en el intento "+ str(intentos))
