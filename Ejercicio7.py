daacb948402ba6d7c893d7309799191e5e18a9e3
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-10-27 00:36:45 +0200

    cambios

--- Ejercicio7.py
+++ Ejercicio7.py
@@ -1,7 +1,12 @@
 def is_leap(year):
     leap = False
-    if((year%=4 ==0) and (year%=100 !=0)and (year%=400 ==0))
+    if(((year % 4) ==0) and ((year% 100) !=0)and ((year% 400) ==0)):
+        leap=True
 # Write your logic here
     return leap
-year = int(input())
-print(is_leap(year))
+year = int(input("Escribe un año y comprueba si es bisiesto o no: "))
+if is_leap(year)== False:
+    print("No es bisiesto el año "+str(year))
+else:
+    print("El año "+str(year) +" si es bisiesto")
+
