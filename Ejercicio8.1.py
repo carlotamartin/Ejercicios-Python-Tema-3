6207d6f7c48485c7b35e98bd5fca7adaae311dd8
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-10-26 12:58:16 +0200

    quitar acentos

--- Ejercicio8.1.py
+++ Ejercicio8.1.py
@@ -2,14 +2,14 @@ import random
 
 num_aleatorio = random.randint(1,15)
 
-num_adivinado = input("Adivina un número entre el 1 y el 15: ")
+num_adivinado = input("Adivina un numero entre el 1 y el 15: ")
 intentos =0
 
 while(num_adivinado != num_aleatorio):
     num_adivinado = input("No has acertado. Prueba de nuevo a escribir un número entre el 1 y el 15: ")
     intentos +=1
     if(num_adivinado<num_aleatorio):
-        print ("El número es mayor")
+        print ("El numero es mayor")
     else:
-        print ("El número es mayor")
-print ("Has acertado el número " + srt(num_aleatorio) + " en el intento "+ str(intentos))
+        print ("El numero es mayor")
+print ("Has acertado el numero " + srt(num_aleatorio) + " en el intento "+ str(intentos))
